b307174ed9b54d591a008bcae87744c8960513a5
durveshyadav3604 <durvesh.yadav3604@gmail.com>
2026-02-13 18:04:49 +0530

    Update main.tf

--- terraformProject/modules/rds/main.tf
+++ terraformProject/modules/rds/main.tf
@@ -7,7 +7,7 @@ resource "random_password" "db_password" {
 
 # create secret
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "rds-mysql-credentials25"
+  name = "rds-mysql-credentials26"
 }
 
 # store username & password
