1d33583147237963d2d767ffb59645f250693855
durveshyadav3604 <durvesh.yadav3604@gmail.com>
2026-02-24 20:19:03 +0530

    Update RDS secret name from credentials30 to credentials31

--- terraformProject/modules/rds/main.tf
+++ terraformProject/modules/rds/main.tf
@@ -7,7 +7,7 @@ resource "random_password" "db_password" {
 
 # create secret
 resource "aws_secretsmanager_secret" "db_secret" {
-  name = "rds-mysql-credentials30"
+  name = "rds-mysql-credentials31"
 }
 
 # store username & password
