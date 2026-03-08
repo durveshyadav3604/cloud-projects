c65f7450145be1efdea1436ae9c42f0d48d41802
durveshyadav3604 <durvesh.yadav3604@gmail.com>
2026-03-08 08:33:16 +0530

    Configure Elastic IP for bastion instance
    Added Elastic IP and association for the bastion instance.

--- terraformProject/modules/bastion/main.tf
+++ terraformProject/modules/bastion/main.tf
@@ -38,20 +38,27 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "aws_instance" "bastion" {
-  ami                         = data.aws_ami.ubuntu2.id
-  instance_type               = var.instance_type
-  subnet_id                   = var.public_subnet_az1_id
-  key_name                    = "linux-key"
-  associate_public_ip_address = true
+  ami           = data.aws_ami.ubuntu2.id
+  instance_type = var.instance_type
+  subnet_id     = var.public_subnet_az1_id
+  key_name      = "linux-key"
+
   user_data = file("${path.root}/userdata1.sh")
 
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.bastion_profile.name
-
+  iam_instance_profile   = aws_iam_instance_profile.bastion_profile.name
 
   tags = {
     Name = "${var.project_name}-public-ec2"
   }
+}
+
+resource "aws_eip" "bastion_eip" {
+  domain = "vpc"
+}
+
+resource "aws_eip_association" "bastion_eip_assoc" {
+  instance_id   = aws_instance.bastion.id
+  allocation_id = aws_eip.bastion_eip.id
 }
